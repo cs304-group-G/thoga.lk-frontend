3750ec39a73f4fdac391496fe31de8bfa9b167c3
Lasind Anjana <lasindua@gmail.com>
2023-10-04 20:41:30 +0530

    basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Loginpage from "./pages/auth/Loginpage";
+import Registerpage from "./pages/auth/Registerpage";
+import Homepage from "./pages/Homepage";
+import Productpage from "./pages/Productpage";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/signin" element={<Loginpage />} />
+      <Route path="/register" element={<Registerpage />} />
+      <Route index element={<Homepage />} />
+      <Route path="/product" element={<Productpage />} />
+    </>
+  )
+);
+
+const App = () => {
+  return <RouterProvider router={router} />;
+};
 
 export default App;
---
+++ src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Homepage = () => {
+  return (
+    <>
+      <Navbar />
+      <div>Homepage</div>
+    </>
+  );
+};
+
+export default Homepage;
