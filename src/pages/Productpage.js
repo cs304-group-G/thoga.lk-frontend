b15dcefaa6fde17dafca78977b711a231babff40
PeminduRayan <pemirayan@gmail.com>
2023-11-03 22:40:26 +0530

    Modified: product tile

--- src/pages/Productpage.js
+++ src/pages/Productpage.js
@@ -13,15 +13,16 @@ const Productpage = () => {
         <div class="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">SALE
         </div>
     </div>
-    <div class="p-4">
-        <h3 class="text-lg font-medium mb-2">Carrot</h3>
-        <h3 class="text-lg font-medium text-gray-600 mb-2">District - Galle</h3>
+    <div class="p-4 ">
+        <span class="text-2xl  font-bold text-center justify-center items-center text-red-800 mb-2">Carrot</span>
+        <span className ='flex flex-row space-x-3' ><h3 class="text-lg font-medium text-gray-600 mb-2">District -</h3>
+        <h3 class="text-lg font-medium text-green-600 mb-2">Galle</h3> </span> 
         <p class="text-gray-600 text-sm mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae ante
             vel eros fermentum faucibus sit amet euismod lorem.</p>
         <div class="flex items-center justify-between">
         <span className = 'space-x-1'>
-        <span class="text-md gap-2 border-[2px] p-2 rounded-md bg-gray-300 font-semibold border-red-500 ">1 Kg</span>
-            <span class="font-bold text-lg">Rs 120.00</span>
+        <span class="text-md gap-2 border-[2px] p-3 rounded-md bg-gray-300 font-semibold border-red-500 ">1 Kg</span>
+            <span class="font-bold text-lg border-gray-400 border-[1px] shadow-lg bg-gray-300 rounded-lg p-3">Rs 120.00</span>
             </span>
             
             <button class="bg-cyan-500 hover:bg-gray-500 text-white font-semibold py-2 px-4 rounded">
